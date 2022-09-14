44f78feabbe89ca63073c57b62e3215a44121b20
GUNN2 <87452860+GUNN2@users.noreply.github.com>
2022-09-14 17:45:52 +1000

    Add files via upload

---
+++ Portfolio/Portfolio/Week3/T1/task1.js
@@ -0,0 +1,14 @@
+var tester = "testing javascript string methods";
+
+console.log (tester.length);
+console.log (tester.toLocaleUpperCase());
+console.log (tester.toLocaleLowerCase());
+
+tester = "                testing javascript string methods";
+
+console.log(tester);
+console.log(tester.trim());
+
+tester = tester.trim();
+
+console.log (tester.substring(19));
